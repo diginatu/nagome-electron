055484d967aaf94ff7d4bce9a3f86e38503966eb
diginatu <diginatu.510o@gmail.com>
2017-11-23 23:36:57 +0900

    add release script and fix package.json

---
+++ RELEASE.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Automatic exit on error
+set -e
+set -o pipefail
+
+ARCHS=("darwin_amd64" "linux_amd64" "windows_amd64")
+TMPDIR="/tmp/nagome_electron_release"
+NPMBIN=$(yarn bin)
+RESOURCEDIR="./app/resources"
+
+mkdir -p $TMPDIR
+$NPMBIN/download-github-release diginatu nagome $TMPDIR
+$NPMBIN/download-github-release diginatu nagome-webapp_server $TMPDIR
+$NPMBIN/download-github-release diginatu nagome-webui $TMPDIR
+
+rm -rf $RESOURCEDIR/
+mkdir -p $RESOURCEDIR
+cp -r $TMPDIR/app $RESOURCEDIR/webui
+
+for arch in ${ARCHS[@]}; do
+    appendexe=$(echo $arch | sed -e '/windows/! d ; /windows/c\.exe')
+    osflag=$(echo $arch | sed -e '/linux/c\--linux' -e '/darwin/c\--mac' -e '/windows/c\--windows')
+    archflag=$(echo $arch | sed -e '/amd64/c\--x64')
+    echo "Creating release for $arch with falg \"$osflag $archflag\""
+
+    cp -r $TMPDIR/nagome_$arch$appendexe $RESOURCEDIR/nagome$appendexe
+    cp -r $TMPDIR/nagome-webapp_server_$arch$appendexe $RESOURCEDIR/server$appendexe
+
+    $NPMBIN/build $osflag $archflag --publish always
+
+    rm -f $RESOURCEDIR/nagome $RESOURCEDIR/server
+done
