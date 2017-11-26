c13427c72c0b129b728ae399afa9ac00b83b2950
diginatu <diginatu.510o@gmail.com>
2017-11-26 15:24:36 +0900

    fix quit Nagome before app quit

--- app/main.js
+++ app/main.js
@@ -63,8 +63,8 @@ app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit();
         nagomeExec.stdin.end();
+        app.quit();
     }
 })
 
