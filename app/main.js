477a8f25bf3858261fcf3c0f1e74ebd3f2015051
diginatu <diginatu.510o@gmail.com>
2018-02-03 15:06:52 +0900

    do not persist app on mac

--- app/main.js
+++ app/main.js
@@ -118,22 +118,7 @@ app.on('window-all-closed', function() {
     uiServerExec.stdin.end();
     uiServerExec = null;
 
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', function() {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow();
-    }
-    if (uiServerExec === null) {
-        executeUIServer();
-    }
+    app.quit();
 });
 
 // In this file you can include the rest of your app's specific main process
